6fd3bafa6e8186cb9931b48f57976e658bd09ec6
heeju00627 <heeju00627@gmail.com>
2017-10-12 03:54:25 +0900

    reinforce mainUi
    new project, add file

--- class_project.py
+++ class_project.py
@@ -5,6 +5,10 @@ Created on Mon Aug  7 14:08:32 2017
 @author: heeju
 """
 
+import class_data as datas
+
+import os
+
 ## Data를 생성, 관리하기 위한 대단위
 ## 프로젝트 생성(폴더) -> fna 파일 추가 -> 저장으로 진행
 ## (status : 1 -> 2 -> 3단계)
@@ -19,12 +23,17 @@ class Project():
         ## 프로젝트 폴더 생성 완료
         self.status = 1
         
+        self.data = datas.Data(0)
+        
     ## 현재 프로젝트 상태 반환
     def getStatus(self):
         return self.status
         
     ####---------------------------------------------
-    ## fna 파일 리스트 추가
-    def addFna():
-        pass
+    ## fna, fasta 파일 리스트 추가
+    def addFiles(self, names):
+        for f in names:
+            if ((os.path.splitext(f)[1][1:]) == ('fna' or 'fasta')):
+                self.data.fileList.append(f);
+        
     
--- mainUi.py
+++ mainUi.py
@@ -14,7 +14,6 @@ import matplotlib.animation as animation
 from matplotlib import style
 
 from threading import Thread
-
 from queue import Queue
 
 import tkinter as tk
@@ -27,6 +26,25 @@ import class_som as somsom
 from minisom import MiniSom
 from numpy import genfromtxt,array,linalg,zeros,mean,std,apply_along_axis
 import numpy
+
+import os
+import re
+
+
+####---------------------------------------------    
+## 팝업 메시지
+def popupMsg(msg):
+    
+    popup = tk.Tk()
+    
+    popup.wm_title("!")
+    label = tk.Label(popup, text=msg)
+    label.grid()
+    
+    button1 = ttk.Button(popup, text="Okay", command=popup.destroy)
+    button1.grid()
+    
+    popup.mainloop()
 
 
 ####---------------------------------------------    
@@ -82,31 +100,56 @@ class newProjectDialog(tk.Toplevel):
         
         self.win.mainloop()
         
+        
+    ####---------------------------------------------    
     def loadFolder(self):
+        
         foldername = tk.filedialog.askdirectory()
         
         if foldername:
             self.entryText.set(foldername)
         
     def onSubmit(self, controller, name, path):
-        newProject = project.Project(name, path)
-        controller.projectList.append(newProject)
         
+        if (not name):
+            popupMsg("프로젝트 이름을 입력하세요")
+        
+        if (re.search(r'[^A-Za-z0-9_\-\\]', name)):
+            popupMsg("올바른 프로젝트 이름을 입력하세요")
+        
+        if (not os.path.isdir(path)):
+            popupMsg("올바른 경로를 입력하세요")
+        
+        if (name and os.path.isdir(path)):
+            newProject = projects.Project(name, path)
+            controller.projectList[name] = newProject
+            controller.projectName.append(name)
+            
+            menu = controller.projectlistmenu.children["menu"]
+            if (controller.projectName[0] == 'no exist'):
+                menu.delete(0, "end")
+                controller.projectName.remove('no exist')
+            menu.add_command(label=name, command=lambda v=name: controller.selectProject(name))
+            controller.selectProject(name)
+            self.win.destroy()
+
+
 
 ####---------------------------------------------    
-## 팝업 메시지
-def popupMsg(msg):
+## add file dialog
+class addFileDialog(tk.Toplevel):
     
-    popup = tk.Tk()
+    def __init__(self, controller):
+        
+        filenames = tk.filedialog.askopenfilenames(filetypes=[('Supported Files(.fna, .fasta)', ('*.fna', '*.fasta')), ('All','*')])
     
-    popup.wm_title("!")
-    label = tk.Label(popup, text=msg)
-    label.grid()
-    
-    button1 = ttk.Button(popup, text="Okay", command=popup.destroy)
-    button1.grid()
-    
-    popup.mainloop()
+        if filenames:
+            controller.projectList[controller.choiceVar.get()].addFiles(filenames)
+            controller.selectProject(controller.choiceVar.get())
+
+
+
+
 
 ####---------------------------------------------    
 ## app 전체 틀
@@ -119,7 +162,8 @@ class app(tk.Tk):
         
         ####---------------------------------------------   
         ## 프로젝트 리스트
-        self.projectList = []
+        self.projectList = {}
+        self.projectName = ['no exist']
         
         ####---------------------------------------------   
         ## 아이콘, 제목
@@ -133,6 +177,17 @@ class app(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         
+        for r in range(7):
+            self.rowconfigure(r, weight=2)
+        self.rowconfigure(0, weight=3)
+        self.rowconfigure(1, weight=1)
+            
+        for c in range(6):
+            self.columnconfigure(c, weight=2)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(5, weight=1)
+        
+        
         ####---------------------------------------------   
         ## 메뉴바
         menubar = tk.Menu(container)
@@ -140,18 +195,17 @@ class app(tk.Tk):
         ## File 메뉴
         fileMenu = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label="File", menu=fileMenu)
-        
-        subProjectMenu = tk.Menu(fileMenu, tearoff=0)
-        subProjectMenu.add_command(label="New Project...",
+        fileMenu.add_command(label="New Project...",
+                                   command=lambda: self.newProject())
+        fileMenu.add_command(label="Load Project...",
                                    command=lambda: popupMsg("not supported just yet"))
-        subProjectMenu.add_command(label="Load Project...",
+        fileMenu.add_command(label="Save Project...",
                                    command=lambda: popupMsg("not supported just yet"))
-        subProjectMenu.add_command(label="Save Project...",
+        fileMenu.add_command(label="Close Project...",
                                    command=lambda: popupMsg("not supported just yet"))
-        subProjectMenu.add_command(label="Close Project...",
-                                   command=lambda: popupMsg("not supported just yet"))
-        fileMenu.add_cascade(label="Project", menu=subProjectMenu)
-        
+        fileMenu.add_separator()
+        fileMenu.add_command(label="Add File...",
+                                 command=lambda: self.addFile())
         fileMenu.add_separator()
         fileMenu.add_command(label="Exit",
                              command=self.destroy)
@@ -185,61 +239,7 @@ class app(tk.Tk):
     
         tk.Tk.config(self, menu=menubar)
         
-        ####---------------------------------------------   
-        ## 상태바
-        status = tk.StringVar()
         
-        statusbar = tk.Label(text="Preparing...", bd=1, relief=tk.SUNKEN, anchor=tk.W)
-        statusbar.grid(row=1, sticky="news")
-        statusbar.grid_columnconfigure(0, weight=1)
-        statusbar.grid_columnconfigure(0, weight=1)
-        
-        ####---------------------------------------------   
-        ## 프레임 관리
-        self.frames = {}
-
-        ## 프레임 생성
-        ## 여러 페이지 돌아야 할 때
-        #for F in (StartPage):
-        F = StartPage
-        frame = F(container, self)
-        self.frames[F] = frame          
-        frame.grid(row=0, column=0, sticky="nsew")
-        frame.grid_rowconfigure(0, weight=1)
-        frame.grid_columnconfigure(0, weight=1)
-        
-        self.show_frame(StartPage)
-
-
-    ####---------------------------------------------   
-    ## 프레임 보여주기
-    def show_frame(self, cont):
-        
-        frame = self.frames[cont]
-        frame.tkraise()
-    
-    
-####---------------------------------------------    
-## 프로젝트 페이지
-## 프로젝트 list 관리
-class StartPage(tk.Frame):
-    
-    ## UI 생성
-    def __init__(self, parent, controller):
-     
-        ## tk.Frame 초기화
-        tk.Frame.__init__(self, parent)
-        
-        ## grid 관리
-        for r in range(7):
-            self.rowconfigure(r, weight=2)
-        self.rowconfigure(0, weight=3)
-        self.rowconfigure(1, weight=1)
-            
-        for c in range(6):
-            self.columnconfigure(c, weight=2)
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(5, weight=1)
         
         ####---------------------------------------------   
         ## 제목
@@ -248,10 +248,11 @@ class StartPage(tk.Frame):
         label.grid_rowconfigure(0, weight=1)
         label.grid_columnconfigure(0, weight=1)
         
+        
         ####---------------------------------------------   
         ## 프로젝트 리스트 관리 버튼(생성, 불러오기, 저장, 닫기)
         button1 = ttk.Button(self, text="New Project..",
-                             command=lambda: self.newProject(controller))
+                             command=lambda: self.newProject())
         button1.grid(row=1, column=1, sticky="ew")
         button1.grid_rowconfigure(0, weight=1)
         button1.grid_columnconfigure(0, weight=1)
@@ -276,34 +277,30 @@ class StartPage(tk.Frame):
         
         ####---------------------------------------------   
         ## 프로젝트 리스트
-        choiceVar = tk.StringVar()
-        #choices = ['no exist project']
-        choices = ['hello']
-        choiceVar.set(choices[0])
+        self.choiceVar = tk.StringVar()
+        self.choiceVar.set(self.projectName[0])
         
         ## 프로젝트 리스트 선택 메뉴
-        projectlistmenu = tk.OptionMenu(self, choiceVar, *choices)
-        projectlistmenu.grid(row=2, column=1, columnspan=4, sticky="ew")
-        projectlistmenu.grid_rowconfigure(0, weight=1)
-        projectlistmenu.grid_columnconfigure(0, weight=1)
+        self.projectlistmenu = tk.OptionMenu(self, self.choiceVar, *self.projectName)
+        self.projectlistmenu.grid(row=2, column=1, columnspan=4, sticky="ew")
+        self.projectlistmenu.grid_rowconfigure(0, weight=1)
+        self.projectlistmenu.grid_columnconfigure(0, weight=1)
         
         ####---------------------------------------------   
         ## 파일 리스트 뷰
-        filelistview = ttk.Treeview(self)
-        filelistview .grid(row=3, rowspan=3, column=1, columnspan=4, sticky="news")
+        self.filelistview = ttk.Treeview(self)
+        self.filelistview .grid(row=3, rowspan=3, column=1, columnspan=4, sticky="news")
     
         ## 파일 리스트 뷰 스크롤바
-        ysb = ttk.Scrollbar(filelistview, orient="vertical", command = filelistview.yview)
-        xsb = ttk.Scrollbar(filelistview, orient="horizontal", command = filelistview.xview)
-        filelistview['yscroll'] = ysb.set
-        filelistview['xscroll'] = xsb.set
+        ysb = ttk.Scrollbar(self.filelistview, orient="vertical", command = self.filelistview.yview)
+        xsb = ttk.Scrollbar(self.filelistview, orient="horizontal", command = self.filelistview.xview)
+        self.filelistview['yscroll'] = ysb.set
+        self.filelistview['xscroll'] = xsb.set
         
-        filelistview.rowconfigure(0, weight=1)
-        filelistview.columnconfigure(0, weight=1)
+        self.filelistview.rowconfigure(0, weight=1)
+        self.filelistview.columnconfigure(0, weight=1)
         ysb.grid(row=0, rowspan=3, column=1, sticky="ns")
         xsb.grid(row=2, column=0, columnspan=2, sticky="ew")
-        
-        filelistview.insert("" , 0, text="GCF_000005845.2_ASM584v2_genomic.fna", values=("fna","4590","no"))
                 
         #tree["columns"]=("one","two")
         #tree.column("one", width=100 )
@@ -320,19 +317,19 @@ class StartPage(tk.Frame):
         #listbox.insert("end", file) 
         
         ## 파일 리스트 뷰 옵션
-        filelistview["columns"] = ("one", "two", "three")
+        self.filelistview["columns"] = ("one", "two", "three")
         
         ## 파일 리스트 뷰 타이틀
-        filelistview.heading("#0", text="name", anchor="w")
-        filelistview.heading("one", text="ext", anchor="w")
-        filelistview.heading("two", text="size", anchor="w")
-        filelistview.heading("three", text="attached", anchor="w")
+        self.filelistview.heading("#0", text="name", anchor="w")
+        self.filelistview.heading("one", text="ext", anchor="w")
+        self.filelistview.heading("two", text="size", anchor="w")
+        self.filelistview.heading("three", text="attached", anchor="w")
         
         ## 파일 리스트 뷰 사이즈
-        filelistview.column("#0", width=200)
-        filelistview.column("one", width=1)
-        filelistview.column("two", width=1)
-        filelistview.column("three", width=1)
+        self.filelistview.column("#0", width=200)
+        self.filelistview.column("one", width=1)
+        self.filelistview.column("two", width=1)
+        self.filelistview.column("three", width=1)
         
         #filelistview.insert("", 0, text="sampleData", values=("1A", "1b"))
         #filelistview.insert("", 3, "dir3", text="Dir 3")
@@ -340,8 +337,8 @@ class StartPage(tk.Frame):
         
         ####---------------------------------------------   
         ## 파일 리스트 관리 버튼(추가, 변환, 트레이닝, 클러스터링)
-        button5 = ttk.Button(self, text="Add",
-                             command=lambda: popupMsg("not supported just yet"))
+        button5 = ttk.Button(self, text="Add File",
+                             command=lambda: self.addFile())
         button5.grid(row=6, column=1, sticky="ew")
         button5.grid_rowconfigure(0, weight=1)
         button5.grid_columnconfigure(0, weight=1)
@@ -366,14 +363,71 @@ class StartPage(tk.Frame):
         
         self.training_count = 0
         self.result_count = 0
-    
-    ## 프로젝트 생성
-    def newProject(self, controller):
-        controller.projectList.append("hi")
         
+        
+        ####---------------------------------------------   
+        ## 상태바
+        self.status = tk.StringVar()
+        self.status.set('Preparing...')
+        
+        self.statusbar = tk.Label(textvariable=self.status, bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        self.statusbar.grid(row=7, column=0, columnspan=6, sticky="news")
+        self.statusbar.grid_columnconfigure(0, weight=1)
+        
+        
+        ####---------------------------------------------   
+        ## 프레임 관리
+        #self.frames = {}
+
+        ## 프레임 생성
+        ## 여러 페이지 돌아야 할 때
+        #for F in (StartPage):
+        #F = StartPage
+        #frame = F(container, self)
+        #self.frames[F] = frame          
+        #frame.grid(row=0, column=0, sticky="nsew")
+        #frame.grid_rowconfigure(0, weight=1)
+        #frame.grid_columnconfigure(0, weight=1)
+        
+        #self.show_frame(StartPage)
+
+
+    ####---------------------------------------------   
+    ## 프레임 보여주기
+    #def show_frame(self, cont):
+        
+    #    frame = self.frames[cont]
+    #    frame.tkraise()
+        
+        
+    ####---------------------------------------------   
+    ## 프로젝트 선택
+    def selectProject(self, name):
+        self.choiceVar.set(name)
+        project = self.projectList[name]
+        
+        self.filelistview.delete(*self.filelistview.get_children())
+        for f in project.data.fileList:
+            self.filelistview.insert("" , 0, text=os.path.basename(f), values=(os.path.splitext(f)[1][1:], "%d KB" % (os.path.getsize(f) / 1024) , "no"))
+        
+        self.status.set(project.path + '/' + project.name)
+        
+        
+    ####---------------------------------------------   
+    ## 프로젝트 생성
+    def newProject(self):
         newProjectDialog(self)
         
-            
+    
+    ####---------------------------------------------
+    ## 파일 추가
+    def addFile(self):
+        if (self.projectName[0] == 'no exist'):
+            popupMsg("열려 있는 프로젝트가 없습니다")
+        else:
+            addFileDialog(self)
+        
+        
     ####---------------------------------------------   
     ## THREAD 생성
     def callback_threaded(self):
@@ -401,6 +455,10 @@ class StartPage(tk.Frame):
         ap2p.grid_columnconfigure(0, weight=1)
         
         ap2p.mainloop()
+        
+    def updateProjects(self):
+        
+        self.choiceVar.set(self.controller.projectName[0])
             
             
 if __name__ == '__main__':
